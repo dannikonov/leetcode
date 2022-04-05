3ff03c07762c25e118ae2c90e54d7569d9f261d9
Danil Nikonov <danil@seventest.ru>
2022-04-05 23:05:40 +0300

    feat: 69. Sqrt(x) (easy)

--- algorithms/php/69.Sqrt(x).php
+++ algorithms/php/69.Sqrt(x).php
@@ -1,22 +1,30 @@
 <?php
 
-function mySqrt($x)
+class Solution
 {
-    $l = 0;
-    $r = $x;
 
-    while ($l < $r) {
-        $m = $l + intdiv($r - $l, 2);
-        if (pow($m, 2) <= $x && $x < pow($m + 1, 2)) {
-            return $m;
-        } else {
-            if (pow($m, 2) < $x) {
-                $l = $m + 1;
+    /**
+     * @param Integer $x
+     * @return Integer
+     */
+    function mySqrt($x)
+    {
+        $l = 0;
+        $r = $x;
+
+        while ($l < $r) {
+            $m = $l + intdiv($r - $l, 2);
+            if (pow($m, 2) <= $x && $x < pow($m + 1, 2)) {
+                return $m;
             } else {
-                $r = $m - 1;
+                if (pow($m, 2) < $x) {
+                    $l = $m + 1;
+                } else {
+                    $r = $m - 1;
+                }
             }
         }
-    }
 
-    return $l;
+        return $l;
+    }
 }
