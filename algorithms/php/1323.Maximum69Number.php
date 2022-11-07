d20373fec8f253b42d905838cd859164cbf121d4
Danil Nikonov <danil@seventest.ru>
2022-11-07 07:02:01 +0300

    feat: 1323. Maximum 69 Number (easy)

--- algorithms/php/1323.Maximum69Number.php
+++ algorithms/php/1323.Maximum69Number.php
@@ -10,7 +10,6 @@ class Solution
     function maximum69Number($num)
     {
         $pos = strpos($num, '6');
-        echo $pos;
         if ($pos !== false) {
             $num = substr_replace($num, '9', $pos, 1);
         }
